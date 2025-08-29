8e1036827221a382816c0e2c3357a15b231bd81c
Hamster <dtaranenko137@gmail.com>
2025-08-29 17:15:14 +0300

    'Solution'

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from cinema.models import Movie
 
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=100, required=True)
--- cinema/urls.py
+++ cinema/urls.py
@@ -5,5 +5,5 @@ from cinema.views import movie_detail, movie_list
 app_name = "cinema"
 urlpatterns = [
     path("movies/", movie_list, name="movie_list"),
-    path("movies/<int:pk>/", movie_detail, name="movie_detail")
+    path("movies/<int:pk>/", movie_detail, name="movie_detail"),
 ]
